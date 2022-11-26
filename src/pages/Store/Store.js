736a8a26d3e69441e424639bce518fc93714ccdb
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-11-26 15:47:33 +0000

    Fetcg Products from DB

--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import initialProducts from "../../initial-products.json";
 import List from "../../components/List";
-import "./store.css"
+import "./store.css";
 
 function Store() {
-  const [products, setProducts] = useState(initialProducts);
+  const [products, setProducts] = useState([]);
+
+  //fetch data using fetch
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      const data = await result.data;
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Products:</h1>
-      <List products={products}/>
+      <List products={products} />
     </>
   );
 }
