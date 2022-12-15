886009071bcc203ae95b68d5491443bdb9cd5c3b
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-12-15 15:37:17 +0000

    Updated Login (useContext not working)

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider } from "./context/AuthProvider.js";
+import UserProvider from "./context/UserContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <Router>
+    <Router>
+      <UserProvider>
         <App />
-      </Router>
-    </AuthProvider>
+      </UserProvider>
+    </Router>
   </React.StrictMode>
 );
 
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,6 +1,7 @@
-import { useEffect, useState, useRef, useContext } from "react";
-import AuthContext from "../../context/AuthProvider.js";
-import { Link } from "react-router-dom";
+import { SERVER_ORIGIN } from "../../consts.js";
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 import axios from "axios";
 import * as MUi from "@mui/material";
 //   Avatar,
@@ -13,151 +14,129 @@ import * as MUi from "@mui/material";
 //   Typography,
 // } from "@mui/material";
 import { LockOutlined, Google } from "@mui/icons-material";
-// import GoogleIcon from "@mui/icons-material/Google";
+
+const defaultUser = {
+  email: "",
+  password: "",
+};
+
+const paperStyle = {
+  padding: 20,
+  minHeight: "40vh",
+  width: 300,
+};
 
 function Login() {
-  const { setAuth } = useContext(AuthContext);
-  // const userRef = useRef();
-  const errRef = useRef();
-
-  const defaultUser = {
-    email: "",
-    password: "",
-  };
   const [user, setUser] = useState(defaultUser);
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
+  const { loginUser } = useContext(UserContext);
 
-  // useEffect(() => {
-  //   useRef.current.focus();
-  // }, []);
+  const sendToServer = async () => {
+    try {
+      const result = await axios.post(
+        SERVER_ORIGIN + "/users/login",
+        JSON.stringify(user),
+        {
+          withCredentials: true,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await result.data;
+      console.log(data);
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [defaultUser]);
-
-  const paperStyle = {
-    padding: 20,
-    minHeight: "40vh",
-    width: 300,
+      loginUser(data);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+    setUser(defaultUser);
   };
 
-  function handleChange(event) {
+  const handleChange = (e) => {
     setUser((old) => {
-      let newValue = event.target.value;
-      return { ...old, [event.target.name]: newValue };
+      let newValue = e.target.value;
+      return { ...old, [e.target.name]: newValue };
     });
-  }
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(user);
-    setSuccess(true);
+  };
 
-    // try {
-    //   const response = await axios.post(
-    //     "http://localhost:5000/users/login",
-    //     JSON.stringify(user),
-    //     { withCredentials: true }
-    //   );
-    //   console.log(JSON.stringify(response?.data));
-    //   // console.log(JSON.stringify(response));
-    //   const accessToken = response?.data?.accessToken;
-    //   const isAdmin = response?.data?.isAdmin;
-    //   setAuth({ user, isAdmin, accessToken });
-    // } catch (err) {
-    //   if (!err?.response) {
-    //     setErrMsg("No server response");
-    //   } else if (err.response?.satus === 400) {
-    //     setErrMsg("Missing name or password");
-    //   } else if (err.response?.satus === 401) {
-    //     setErrMsg("Unauthorized");
-    //   } else {
-    //     setErrMsg("Login Failed");
-    //   }
-    //   // errRef.current.focus();
-    // }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    sendToServer();
   };
 
   function googleAuth() {
-    window.location.href = `http://localhost:5000/users/google`;
+    window.location.href = SERVER_ORIGIN + "/users/google";
   }
 
   return (
     <>
-      {success ? (
-        <section>
-          <h1>You are logged in!</h1>
-        </section>
-      ) : (
-        <MUi.Grid
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          minHeight={"100vh"}
-          margin="0"
-        >
-          <MUi.Paper elevation={3} style={paperStyle}>
-            <MUi.Grid align="center">
-              <MUi.Avatar>
-                <LockOutlined />
-              </MUi.Avatar>
-              <h2>Login</h2>
-              <form onSubmit={handleSubmit}>
-                <MUi.FormControl variant="standard" fullWidth>
-                  <MUi.InputLabel>Email</MUi.InputLabel>
-                  <MUi.Input
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange}
-                    // ref={userRef}
-                  />
-                </MUi.FormControl>
+      <MUi.Grid
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        minHeight={"100vh"}
+        margin="0"
+      >
+        <MUi.Paper elevation={3} style={paperStyle}>
+          <MUi.Grid align="center">
+            <MUi.Avatar>
+              <LockOutlined />
+            </MUi.Avatar>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+              <MUi.FormControl variant="standard" fullWidth>
+                <MUi.InputLabel>Email</MUi.InputLabel>
+                <MUi.Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={user.email}
+                />
+              </MUi.FormControl>
 
-                <MUi.FormControl variant="standard" fullWidth>
-                  <MUi.InputLabel>Password</MUi.InputLabel>
-                  <MUi.Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                  />
-                </MUi.FormControl>
+              <MUi.FormControl variant="standard" fullWidth>
+                <MUi.InputLabel>Password</MUi.InputLabel>
+                <MUi.Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={user.password}
+                />
+              </MUi.FormControl>
 
-                <p ref={errRef}>{errMsg}</p>
-
-                <MUi.Button
-                  variant="contained"
-                  type="submit"
-                  sx={{
-                    background: "#ff5151",
-                    color: "white",
-                    margin: "2em 0",
-                  }}
-                  fullWidth
-                >
-                  Login
-                </MUi.Button>
-                <MUi.Button
-                  onClick={googleAuth}
-                  variant="contained"
-                  type="button"
-                  sx={{ background: "", color: "white", margin: "1em 0" }}
-                  fullWidth
-                >
-                  {" "}
-                  <Google /> Login with Google
-                </MUi.Button>
-                <MUi.Typography>
-                  Don't have an account? <Link to={"/signup"}>Sign up</Link>
-                </MUi.Typography>
-              </form>
-            </MUi.Grid>
-          </MUi.Paper>
-        </MUi.Grid>
-      )}
+              <MUi.Button
+                variant="contained"
+                type="submit"
+                sx={{
+                  background: "#ff5151",
+                  color: "white",
+                  margin: "1em 0",
+                }}
+                fullWidth
+              >
+                Login
+              </MUi.Button>
+              <MUi.Button
+                onClick={googleAuth}
+                variant="contained"
+                type="button"
+                sx={{ background: "", color: "white", margin: "1em 0" }}
+                fullWidth
+              >
+                <Google /> Login with Google
+              </MUi.Button>
+              <MUi.Typography>
+                Don't have an account? <Link to={"/signup"}>Signup</Link>
+              </MUi.Typography>
+            </form>
+          </MUi.Grid>
+        </MUi.Paper>
+      </MUi.Grid>
     </>
   );
 }
