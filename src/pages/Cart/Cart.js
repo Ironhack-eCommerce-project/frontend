e67de09ff08c63d0e2d253f753703b0e68a58ec2
Patrick-Groot <patrick@candis.io>
2022-12-17 11:59:47 +0100

    frontend up-to-date

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,19 @@
+function Cart({ cartProducts, setCartProducts }) {
+    console.log(cartProducts);
+    return (
+      <>
+        <h2>Cart:</h2>
+        <ul>
+          {cartProducts.map((elem) => {
+            return (
+              <div>
+                <h3>{elem.name}</h3>
+              </div>
+            )
+          })}
+        </ul>
+      </>
+    );
+  }
+  
+export default Cart;
