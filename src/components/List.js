377bbb4cb0928f4778cc7ccbd033c7004b9789f5
Patrick-Groot <patrick@candis.io>
2023-01-10 19:58:37 +0100

    Improve putting items into Cart

--- src/components/List.js
+++ src/components/List.js
@@ -12,24 +12,42 @@ import {
 } from "@mui/material";
 import "../pages/Store/store.css";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function List({ products }) {
-  console.log("Products list", products);
-  const [addCartProducts, setAddCartProducts] = useState([]);
+function List({ products, setProductsInCart, productsInCart }) {
+  const [productToCart, setProductToCart] = useState({});
 
   const handleClick = async (e) => {
     e.preventDefault();
-    console.log(e.target.id);
     const addedProduct = e.target.id;
-    console.log("addedProd", addedProduct);
-    setAddCartProducts([...addCartProducts, addedProduct]);
-    console.log("CartProducts", addCartProducts);
-    const resp = await axios.post("/cart", addCartProducts, {
+    setProductToCart({ addedProduct });
+    const resp = await axios.post("/cart", productToCart, {
       withCredentials: true,
     });
     console.log("resp:", resp);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/cart");
+      const data = await result.data;
+      console.log("Refresh Products in Cart");
+      setProductsInCart(data);
+    };
+    fetchData();
+  }, [setProductsInCart, productToCart]);
+
+  useEffect(
+    () =>
+      console.log(
+        "PROD IN CART: ",
+        productsInCart,
+        productsInCart.length,
+        "Add Cart Prod: ",
+        productToCart
+      ),
+    [productsInCart, productToCart]
+  );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -40,7 +58,12 @@ function List({ products }) {
               <Grid item xs={12} sm={6} md={4} lg={4} key={elem.slug}>
                 <Card raised>
                   <CardActionArea component={Link} to={`/store/${elem.slug}`}>
-                    <CardMedia component="img" height="260" image={elem.image} alt={elem.name} />
+                    <CardMedia
+                      component="img"
+                      height="260"
+                      image={elem.image}
+                      alt={elem.name}
+                    />
                     {/* <Link to={`/store/${elem.slug}`}>
                       <img src={elem.image} alt={elem.name} className="listItemImg" />
                     </Link> */}
@@ -48,10 +71,16 @@ function List({ products }) {
                       <Typography variant="body" color="text.secondary" noWrap>
                         {elem.name}
                       </Typography>
-                      <Typography variant="subtitle2" color="text.secondary" noWrap>
+                      <Typography
+                        variant="subtitle2"
+                        color="text.secondary"
+                        noWrap
+                      >
                         {elem.category.name}
                       </Typography>
-                      <CardActions sx={{ display: "flex", justifyContent: "space-around" }}>
+                      <CardActions
+                        sx={{ display: "flex", justifyContent: "space-around" }}
+                      >
                         <Button
                           size="small"
                           variant="contained"
@@ -61,7 +90,11 @@ function List({ products }) {
                         >
                           Add to Cart
                         </Button>
-                        <Typography variant="body1" color="text.secondary" align="right">
+                        <Typography
+                          variant="body1"
+                          color="text.secondary"
+                          align="right"
+                        >
                           {`${elem.price} €`}
                         </Typography>
                       </CardActions>
