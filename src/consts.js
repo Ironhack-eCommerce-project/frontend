c6824c0a4cb8ec4bbdae6a912305be0fcb5980b5
Patrick-Groot <patrick@candis.io>
2023-01-14 11:39:01 +0100

    change consts

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
-export const SERVER_ORIGIN = process.env.REACT_APP_SERVER_ORIGIN || "http:fgfgf:5000";
+export const SERVER_ORIGIN = process.env.REACT_APP_SERVER_ORIGIN || "http:localhost:5000";
 console.log("server_origin:", SERVER_ORIGIN);
