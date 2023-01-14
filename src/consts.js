e8b7ccb19ec9bede291f1e3590769323c515c38e
Patrick-Groot <patrick@candis.io>
2023-01-14 11:33:29 +0100

    test

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
-export const SERVER_ORIGIN = process.env.SERVER_ORIGIN || "http:localhost:5000";
+export const SERVER_ORIGIN = process.env.SERVER_ORIGIN || "http:fgfgf:5000";
 console.log("server_origin:", SERVER_ORIGIN);
