5debd7cee37a1637e86c37deab570d6164c129a9
JoaoSerafim3001 <seafim.joao@gmail.com>
2023-01-14 10:15:18 +0000

    updated SERVER_ORIGIN

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
-export const SERVER_ORIGIN = process.env.SERVER_ORIGIN || "http:localhost:5000";
+export const SERVER_ORIGIN = process.env.REACT_APP_SERVER_ORIGIN || "http:localhost:5000";
 console.log("SERVER_ORIGIN");
